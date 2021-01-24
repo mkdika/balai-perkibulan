1bfac6d136cf3309288aabd47eab7bc8ed461b18
Maikel Chandika <mkdika@gmail.com>
2021-01-24 08:30:58 +0700

    Finish login screen visual adjustment

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :omniauthable, omniauth_providers: [:google_oauth2]
 
   def self.from_omniauth(auth)
-    binding.pry
     user = where(email: auth.info.email).first
 
     if user.present?
-      user.update_attributes(display_name: auth.info.name, image: auth.info.image)
+      user.update(display_name: auth.info.name, image: auth.info.image)
     else
       user = User.create(email: auth.info.email, provider: auth.provider, uid: auth.uid, display_name: auth.info.name, image: auth.info.image)
     end
